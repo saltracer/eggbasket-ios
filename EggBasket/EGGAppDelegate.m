3ba17832cd04c0a7c87e00badf55dbc9fba6f163
Paul Swagerty <pswagerty@gmail.com>
2014-06-13 17:06:11 -0700

    Getting rootViewController set

--- EggBasket/EGGAppDelegate.m
+++ EggBasket/EGGAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "EGGAppDelegate.h"
+#import "EGGAddClutchTableViewController.h"
 
 @implementation EGGAppDelegate
 
@@ -17,9 +18,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    
+    EGGAddClutchTableViewController *viewController = [[EGGAddClutchTableViewController alloc] init];
+    self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
---
+++ EggBasket/ViewControllers/EGGAddClutchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EGGAddClutchTableViewController.h
+//  EggBasket
+//
+//  Created by Paul Swagerty on 6/13/14.
+//  Copyright (c) 2014 com.backyardeggbasket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EGGAddClutchTableViewController : UITableViewController
+
+@end
